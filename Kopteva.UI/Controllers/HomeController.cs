3dca6e35c8855309f42e7cc108fa39f47bccac80
PK <PK@DESKTOP-11UFS25>
2026-06-16 16:49:52 +0300

    Initial commit with Lab_1

---
+++ Kopteva.UI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kopteva.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
